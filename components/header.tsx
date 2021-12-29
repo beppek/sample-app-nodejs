19425f0cc8e5bd81482bf81feecd62afc677df7a
Max Karlsson <beppe@live.se>
2021-12-29 14:57:43 +1100

    refactor: colocate header components

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,8 @@
-import { Box, Tabs } from '@bigcommerce/big-design';
+import { Box, Button, H1, HR, Tabs, Text } from '@bigcommerce/big-design';
+import { ArrowBackIcon } from '@bigcommerce/big-design-icons';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
-import InnerHeader from './innerHeader';
+import { useProductList } from '../lib/hooks';
 
 export const TabIds = {
     HOME: 'home',
@@ -28,6 +29,28 @@ const HeaderTypes = {
     INNER: 'inner',
     HEADERLESS: 'headerless',
 };
+
+const InnerHeader = () => {
+    const router = useRouter();
+    const { pid } = router.query;
+    const { list = [] } = useProductList();
+    const { name } = list.find(item => item.id === Number(pid)) ?? {};
+
+    const handleBackClick = () => router.push(TabRoutes[TabIds.PRODUCTS]);
+
+    return (
+        <Box marginBottom="xxLarge">
+            <Button iconLeft={<ArrowBackIcon color="secondary50" />} variant="subtle" onClick={handleBackClick}>
+                <Text bold color="secondary50">Products</Text>
+            </Button>
+            {name &&
+                <H1>{name}</H1>
+            }
+            <HR color="secondary30" />
+        </Box>
+    );
+};
+
 
 const Header = () => {
     const [activeTab, setActiveTab] = useState<string>('');
